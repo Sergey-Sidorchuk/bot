5cf41eb8294a77b09b7f67ef7908bbdfa6ef3e7f
Serg-SS <serg_ss@ukr.net>
2021-02-24 01:40:38 +0200

    module3/task 12

---
+++ jsmodule3/12.js
@@ -0,0 +1,30 @@
+// Задача. Подсчёт свойств
+// Задание
+// Напиши функцию countProps(object), которая считает и возвращает количество собственных свойств объекта в параметре object. Используй переменную propCount для хранения количества свойств объекта.
+
+// Тесты
+// Объявлена функция countProps(object).
+// Вызов countProps({}) возвращает 0.
+// Вызов countProps({ name: 'Mango', age: 2 }) возвращает 2.
+// Вызов countProps{ mail: 'poly@mail.com', isOnline: true, score: 500 }) возвращает 3.
+// Функция подсчитывает только собственные свойства объекта.
+
+function countProps(object) {
+  let propCount = 0;
+  // Пиши код ниже этой строки
+for (const key of Object.keys(object)) {
+  if (object.hasOwnProperty(key)) {
+    propCount = Object.keys(object).length;
+  }
+  }
+  
+  // Пиши код выше этой строки
+  return propCount;
+}
+// console.log(propCount);
+console.log(countProps({}));
+console.log(countProps({ name: 'Mango', age: 2 }));
+console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 }));
+
+
+
---
+++ jsmodule3/13.js
@@ -0,0 +1,17 @@
+
+apartment = {
+  descr: 'Просторная квартира в центре',
+  rating: 4,
+  price: 2153,
+};
+const values = [];
+// Пиши код ниже этой строки
+const keys = Object.keys(apartment);
+  console.log(keys);
+
+for (const value of values){
+  values.push([value]);
+  
+console.log(values);
+}
+
